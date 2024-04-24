254895f2f05f3b62939ab0f17085bd3e961afc09
suin4404 <suin4404@naver.com>
2024-04-24 16:29:09 +0900

    btnCall, menuMo 추가하였음

--- script/main.js
+++ script/main.js
@@ -2,7 +2,9 @@ let btnCall = document.querySelector('.btnCall');
 let menuMo = document.querySelector('.menuMo');
 
 btnCall.addEventListener('click', (e) => {
+	// e = 이벤트 객체
 	e.preventDefault();
 	btnCall.classList.toggle('on');
+	// 토글 = 스위치
 	menuMo.classList.toggle('on');
 });
